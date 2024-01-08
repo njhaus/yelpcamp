6ba796abe79c82c010432f8b8c818a7697d83ee4
njhaus <njhaus@gmail.com>
2024-01-08 15:09:52 -0600

    update maptiler

--- public/campgrounds/index-map.mjs
+++ public/campgrounds/index-map.mjs
@@ -118,7 +118,7 @@ const createMap = (data, key) => {
 
   const map = new maplibregl.Map({
     container: container,
-    style: `https://api.maptiler.com/maps/streets/style.json?key=2XZKg54dnt7JS7AZhe7J`,
+    style: `https://api.maptiler.com/maps/streets/style.json?key=TlHNy4yOvdMALRIOY30Y`,
     center: searchCoords,
     zoom: zoom,
   });
